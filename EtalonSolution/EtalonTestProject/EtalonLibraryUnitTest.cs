a73a849edb7697314932d45a69f83c2130df2d17
viktor novikov <git@git.ilook.ru:lingvo>
2014-10-17 14:28:56 +0400

    Added EtalonTest project for automatic unit testing.

---
+++ EtalonSolution/EtalonTestProject/EtalonLibraryUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EtalonLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EtalonTestProject
+{
+    [TestClass]
+    public class EtalonLibraryUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var tester = (EtalonNamedObject)EtalonFactory.Instance();
+            var name = tester.Name;
+            const string expectedName = "DefaultName";
+            Assert.AreEqual(expectedName, name);
+        }
+    }
+}
